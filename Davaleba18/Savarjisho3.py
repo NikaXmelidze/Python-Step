570e3107e988442f1343025faaa4bbeff0d6ff65
Nika <xmela12@gmail.com>
2024-02-21 18:44:01 +0400

    gavaswore me18 davalebis me3 savarjisho

--- Davaleba18/Savarjisho3.py
+++ Davaleba18/Savarjisho3.py
@@ -2,9 +2,8 @@
 def decorator(func):
     def wrapper(*args):
         balance = args[0]
-        amount = args[1] 
-        commision = 1
-        if balance - amount - commision< 0:
+        amount = args[1] + 1
+        if balance - amount< 0:
             print("The funds on your account are not enough to complete this transaction")
         else:
             func(balance, amount)
@@ -16,7 +15,7 @@ def transaction(balance, amount):
 
     print(f"Current balance: {balance}")
     balance -= amount
-    print(f"{amount} has been transferred. New balance after commision {balance-1}")
+    print(f"{amount} has been transferred. New balance after commision {balance}")
 
 
 
